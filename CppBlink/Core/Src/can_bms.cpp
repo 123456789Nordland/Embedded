e25f1ce90522410b6d0a6835d7a5a9a823b27ff0
Alexander <maltxwest@gmail.com>
2025-08-10 22:32:30 +0200

    Ertses_Projekt_C++

---
+++ CppBlink/Core/Src/can_bms.cpp
@@ -0,0 +1,27 @@
+/*
+ * can_bms.cpp
+ *
+ *  Created on: Aug 4, 2025
+ *      Author: malya
+ */
+
+
+
+
+#include "can_bms.h"
+// ggf. auch HAL oder LL Includes
+
+UART::UART() {
+    // Konstruktorcode
+}
+
+void UART::init() {
+    // GPIO/UART konfigurieren
+}
+
+void UART::sendChar(char c) {
+    // Zeichen senden
+}
+
+
+
